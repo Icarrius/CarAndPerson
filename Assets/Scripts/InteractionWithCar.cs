3ecb90fdfb548bca03fae895513f28eb5064a9c1
ТАРАСОВ АЛЕКСАНДР <tarasov.a57@wildberries.work>
2024-04-13 13:29:04 +0300

    Update

--- Assets/Scripts/InteractionWithCar.cs
+++ Assets/Scripts/InteractionWithCar.cs
@@ -7,6 +7,8 @@ public class InteractionWithCar : MonoBehaviour
     public GameObject hint;
 
     private bool isOnCar = false;
+    private bool inCar = false;
+
     private CarMovement car;
 
     private void Start()
@@ -17,17 +19,46 @@ public class InteractionWithCar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E) && isOnCar)
+        if(Input.GetKeyDown(KeyCode.E) && isOnCar && !inCar)
         {
             // 1 Включить управление машиной
             car.isActive = true;
 
-            // 2. Включить физику машинке
-            car.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-            car.GetComponent<CapsuleCollider2D>().enabled = true;
+            // 3 Перенести персонажа внутрь машинки
+            transform.SetParent(car.transform);
 
-            // 3 Скрыть персонажа
-            gameObject.SetActive(false);
+            // 4 Скрыть персонажа
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+
+            // 5 Выключить управление персонажем
+            gameObject.GetComponent<PlayerMovement>().isActive = false;
+
+            inCar = true;
+
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && inCar)
+        {
+            // Включить управление машиной
+            car.isActive = false;
+
+            // Включить управление персонажем
+            gameObject.GetComponent<PlayerMovement>().isActive = true;
+
+            // Активируем персонажа
+            gameObject.GetComponent<SpriteRenderer>().enabled = true;
+            gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+
+            // Перенести персонажа из машинки
+            transform.SetParent(null);
+
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+
+            inCar = false;
+
+            return;
         }
     }
 
